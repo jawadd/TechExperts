cca90a54ef33fe845068b9cb94b5417ddfaefc3d
Jawad Hussain <jawadduet@gmail.com>
2023-01-08 21:53:21 +0500

    Added Reviews model

--- projects/models.py
+++ projects/models.py
@@ -14,3 +14,19 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Review(models.Model):
+    VOTE_TYPE = (
+        ('up', 'Up Vote'),
+        ('down', 'Down Vote'),
+    )
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=200, choices=VOTE_TYPE)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.value
