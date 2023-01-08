00b789b78ec0ff4ecb9ebd190fbe263cea0caee1
Jawad Hussain <jawadduet@gmail.com>
2023-01-08 22:04:49 +0500

    added and modified models
        - Create model Tag
        - Add field vote_ratio to project
        - Add field vote_total to project
        - Create model Review
        - Add field tag to project

--- projects/models.py
+++ projects/models.py
@@ -8,6 +8,9 @@ class Project(models.Model):
     description = models.TextField(null=True, blank=True)
     demo_link = models.CharField(max_length=2000, null=True, blank=True)
     source_link = models.CharField(max_length=2000, null=True, blank=True)
+    tag = models.ManyToManyField('Tag', blank=True)
+    vote_total = models.IntegerField(default=0, null=True, blank=True)
+    vote_ratio = models.IntegerField(default=0, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True,
                           primary_key=True, editable=False)
@@ -30,3 +33,13 @@ class Review(models.Model):
 
     def __str__(self):
         return self.value
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
